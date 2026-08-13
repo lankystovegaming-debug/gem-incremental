2928a8961c7292e40d208018887db7ded116585f
lankystovegaming-debug <lankystovegaming@gmail.com>
2026-08-13 08:38:51 +0800

    Update supabase.js

--- src/backend/supabase.js
+++ src/backend/supabase.js
@@ -2,11 +2,14 @@ import {
   createClient
 } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL =
+
+export const SUPABASE_URL =
   "https://fuzidbblwzrhhbonjqjm.supabase.co";
 
-const SUPABASE_PUBLISHABLE_KEY =
+
+export const SUPABASE_PUBLISHABLE_KEY =
   "sb_publishable_sCiiZxMP5DG8_gjGLg9qUg_HnWVD27U";
+
 
 export const supabase =
   createClient(
